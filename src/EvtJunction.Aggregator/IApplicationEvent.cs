95e2c99053fc157390696bcb2ae71fd0c5424da8
Brian Hall <brian@nebbiatech.com>
2015-06-13 13:58:32 -0400

    Added acknowledgements to the original author Matthias Jauernig in all code files.

--- src/EvtJunction.Aggregator/IApplicationEvent.cs
+++ src/EvtJunction.Aggregator/IApplicationEvent.cs
@@ -1,4 +1,11 @@
-﻿namespace EvtJunction.Aggregator
+﻿//////////////////////////////////////////
+// This code originally done by Matthias Jauernig as part of a blog post about an implmentation of the Event Aggregator pattern.
+// Slight modifications have been done by Brian Hall.
+// Original blog post entry:
+// http://www.minddriven.de/index.php/technology/development/design-patterns/event-aggregator-implementation
+//////////////////////////////////////////
+
+namespace EvtJunction.Aggregator
 {
 	public interface IApplicationEvent
 	{
--- src/EvtJunction.Aggregator/Subscription.cs
+++ src/EvtJunction.Aggregator/Subscription.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿//////////////////////////////////////////
+// This code originally done by Matthias Jauernig as part of a blog post about an implmentation of the Event Aggregator pattern.
+// Slight modifications have been done by Brian Hall.
+// Original blog post entry:
+// http://www.minddriven.de/index.php/technology/development/design-patterns/event-aggregator-implementation
+//////////////////////////////////////////
+
+using System;
 
 namespace EvtJunction.Aggregator
 {
